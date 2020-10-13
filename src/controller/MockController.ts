36855cd80defa4c9178a5755a7909e23d28f7de4
Aleks <alec.augal@gmail.com>
2020-10-13 20:09:40 +0200

    Added linter and applied style (#3)
    * Added eslint and applied the code style
    
    * Solved linter errors
    
    * Updated the typescript version to 4.0.3 version

--- src/controller/MockController.ts
+++ src/controller/MockController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
-import { Project } from "api-builder-types";
+import { Request, Response } from 'express';
+import { Project } from 'api-builder-types';
 
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 
 /**
  * Loads all posts from the database.
@@ -9,7 +9,7 @@ const fs = require("fs").promises;
 export const ping = async (request: Request, response: Response) => {
     response.setHeader('Content-type', 'application/json');
     response.send(JSON.stringify({
-        Response: "Server alive and working"
+        Response: 'Server alive and working',
     }));
 };
 
@@ -17,10 +17,10 @@ export const getProjects = async (request: Request, response: Response) => {
     response.setHeader('Content-type', 'application/json');
     await fs.readFile('./src/ExampleConfigs/Projects.json').then((rawData: Buffer) => {
         const rawDataString = rawData.toString();
-        const jsonToSend: Project[] = JSON.parse(rawDataString)["Projects"];
+        const jsonToSend: Project[] = JSON.parse(rawDataString).Projects;
         response.send(jsonToSend);
     }).catch((error) => {
-        const jsonToSend = JSON.stringify({ "ERROR": error});
+        const jsonToSend = JSON.stringify({ ERROR: error });
         response.status(500).send(jsonToSend);
     });
 };
